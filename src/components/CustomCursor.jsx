de2076498a3a75ffdc213313338d03031e50942f
sarvesh0506 <sarveshkumarsps206@gmail.com>
2026-07-18 00:17:12 +0530

    UI: Change custom mouse cursor pointer to Coca-Cola circular disc logo SVG

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -30,11 +30,16 @@ export default function CustomCursor() {
 
   return (
     <motion.div
-      className="fixed top-0 left-0 w-5 h-5 rounded-full bg-coca-red pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] shadow-[0_0_15px_rgba(228,30,38,0.9)]"
+      className="fixed top-0 left-0 w-9 h-9 pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] drop-shadow-[0_0_12px_rgba(228,30,38,0.85)]"
       style={{
         left: cursorX,
         top: cursorY,
       }}
-    />
+    >
+      <svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
+        <circle cx="50" cy="50" r="46" fill="#E41E26" stroke="#FFFFFF" stroke-width="2" />
+        <path d="M15,62 C30,62 35,38 52,38 C68,38 72,52 85,42 C75,54 65,58 52,58 C38,58 30,48 15,62 Z" fill="#FFFFFF" />
+      </svg>
+    </motion.div>
   );
 }
